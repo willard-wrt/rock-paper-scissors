882661019c59a3d7fbdc58432832ea15efe571bf
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2022-12-09 19:36:53 +0700

    Add a conditional function that will determine the winner

--- script.js
+++ script.js
@@ -12,3 +12,47 @@ function getComputerChoice(){
   }
   return cpuChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+
+  let result = "";
+  if (playerSelection.toLowerCase() == "rock" && computerSelection == "Rock") {
+    result = "It's a draw"
+  }
+  else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
+    result = "You lost!"
+  } 
+  else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+    result = "You won!"
+  } 
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
+    result = "You won!"
+  }
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Paper") {
+    result = "It's a draw"
+  } 
+  else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
+    result = "You lost!"
+  } 
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
+    result = "You lost!"
+  }
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
+    result = "You won!"
+  } 
+  else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Scissors") {
+    result = "It's a draw"
+  } 
+  return result;
+  }
+
+
+  const computerSelection = getComputerChoice();  
+  const playerSelection = prompt("Please type 'Rock', 'Paper' or 'Scissors' ");
+  const capitalized = playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1).toLowerCase();
+   
+  console.log("You choose:" + " " + capitalized);
+  console.log("CPU chooses:" + " " + computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
+  
+
